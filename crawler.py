d8665534df4c54fc39205e748183020152b4de22
CapitanAndrea <andrea.michienzi@libero.it>
2017-06-08 18:04:34 +0200

    Major crawler update
    .) The crawler now crawls wikipedia pages

--- crawler.py
+++ crawler.py
@@ -1,4 +1,5 @@
 import urllib2
+import re
 import ssl
 import time
 import json
@@ -42,36 +43,87 @@ def parse_routes(routes_object):
 			edges[destination]=(count+1)
 	return edges	
 
+def req_page(page_name):
+	#wait enough time to get the request accepted
+	time.sleep(1)
+	#send the request
+	gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+	page_utf=page_name.encode('utf-8', 'replace')
+	print page_utf
+	page=urllib2.urlopen('https://en.wikipedia.org/w/api.php?action=query&titles='+page_utf+'&prop=revisions&rvprop=content&format=json', context=gcontext).read()
+	jdata=json.loads(page)
+	
+	response=jdata['query']['pages']
+	page_id=response.keys()[0]
+	if response[page_id].has_key('missing'): return {}
+	page_content=response[page_id]['revisions'][0]['*']
+	if page_content.startswith('#'):
+		regex = ur"\[\[(.+?)\]\]+?"
+		page_id = re.findall(regex, page_content)[0]
+		return req_page(page_id.replace(' ', '_'))
+	
+	#store the result in a file
+	out_file = open("pages/"+page_name,"w")
+	out_file.write(beautify(page))
+	out_file.close()
+	#return the string itself
+	return jdata
+
+def parse_page(page_object):
+	flights={}
+	edges={}
+	#transform the json into a python object
+	response=page_object['query']['pages']
+	page_id=response.keys()[0]
+	page_content=response[page_id]['revisions'][0]['*']
+	regex = ur"\[\[(.+?)\]\]+?"
+	linked_pages = re.findall(regex, page_content)
+
+	#retrieve all the routes
+	for i in range(len(linked_pages)):
+		linked_page=linked_pages[i]
+		if not linked_page.find('|')==-1:
+			linked_page=linked_page.split('|')[0]
+		linked_page=linked_page.replace(' ', '_')
+		count=edges.get(linked_page, 0)
+		edges[linked_page]=(count+1)
+	return edges
+
 #starting seed is PSA (Pisa international airport)
-airports_map={}
-airports_map['PSA']=0
-airports_queue=Queue.Queue()
-airports_queue.put('PSA')
+if not os.path.exists('pages'):
+    os.makedirs('pages')
+page_map={}
+page_map['barabasi']=0
+page_queue=Queue.Queue()
+page_queue.put('barabasi')
 #for each of the airports, find the routes starting from the airport
 out_file = open("graph","w")
 out_file_id=open("graph_id", "w")
 out_file_id.write("# source_id destination_id weight\n")
 num=0
+
 #while the queue is not empty
-while not airports_queue.empty():
+while not page_queue.empty():
 	num=num+1
 	#get the next airport id, and ask for its routes
-	airport_code=airports_queue.get()
-	routes=req_flights(airport_code)
-	edges=parse_routes(routes)
+	page_name=page_queue.get()
+	page_obj=req_page(page_name)
+	if(page_obj=={}): continue
+	edges=parse_page(page_obj)
 	#write all the newly found routes
 	for j in edges.items():
-		out_file.write(airport_code+"\t"+j[0]+"\t"+str(j[1])+"\n")
+		out_file.write(page_name.encode('ascii', 'replace')+"\t"+j[0].encode('ascii', 'replace')+"\t"+str(j[1])+"\n")
 		#if a new airport is found, assign it a new private id number and put it in the queue
-		if not airports_map.has_key(j[0]):
-			airports_map[j[0]]=len(airports_map)
-			airports_queue.put(j[0])
-		out_file_id.write(str(airports_map[airport_code])+"\t"+str(airports_map[j[0]])+"\t"+str(j[1])+"\n")
+		if not page_map.has_key(j[0]):
+			page_map[j[0]]=len(page_map)
+			page_queue.put(j[0])
+		out_file_id.write(str(page_map[page_name])+"\t"+str(page_map[j[0]])+"\t"+str(j[1])+"\n")
 	print(str(num))
+	if num>5000: break
 out_file.close()
 out_file_id.close()
 
 #also write to file the dictionary with iata code-private id association
-out_file = open("pages/airports","w")
-out_file.write(str(airports_map))
+out_file = open("pages/pages","w")
+out_file.write(str(page_map))
 out_file.close()
