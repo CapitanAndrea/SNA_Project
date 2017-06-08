2ec8b429c52a4bf9c88ffd9e9a62eb29f9ea3cd9
CapitanAndrea <andrea.michienzi@libero.it>
2017-06-08 09:43:57 +0200

    Modifications on the crawler
    -) The crawler asks no more for the full airport list
    .) It instead starts the crawl from an arbitrary airport (currently, the Pisa International Airport)
    .) The logic of the crawler has changed: now it gets knowledge of the airports as it crawls the routes and it keeps track of the airports to visit using a queue
    -) All the code about the request for the airport list has been removed

--- crawler.py
+++ crawler.py
@@ -2,6 +2,7 @@ import urllib2
 import ssl
 import time
 import json
+import Queue
 
 #indent a json string to be more human readable
 def beautify(json_string):
@@ -31,51 +32,43 @@ def parse_routes(routes_json):
 	#retrieve all the routes
 	for i in range(len(response)):
 		flight=response[i]
-		if flights.has_key(flight["flight_number"]):
-			continue
-		else:
+		if not flights.has_key(flight["flight_number"]):
 			flights[flight["flight_number"]]=1
 			destination=flight["arrival"]
 			count=edges.get(destination, 0)
 			edges[destination]=(count+1)
 	return edges	
 
-#parse the airport response from the web service
-def parse_airports(airports_json):
-	airports_array={}
-	#transform the json into a python object
-	jdata=json.loads(airports_json)
-	response=jdata['response']
-	#retrieve all the iata codes of the airports
-	for i in range(10051):
-		airports_array[response[i]['code']]=i
-	return airports_array
-
-#request info of all the available airports
-gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
-airports=urllib2.urlopen('https://iatacodes.org/api/v6/airports?api_key=7a6ff549-3dcc-45ad-9c10-37bd07b0b411', context=gcontext).read()
-#store the json in a file
-out_file = open("pages/airports","w")
-out_file.write(beautify(airports))
-out_file.close()
-#retrieve all the airports iata codes
-airports_array=parse_airports(airports)
-"""
-for i in range(10053):
-	print(airports_array[i])
-"""
+#starting seed is PSA (Pisa international airport)
+airports_map={}
+airports_map['PSA']=0
+airports_queue=Queue.Queue()
+airports_queue.put('PSA')
 #for each of the airports, find the routes starting from the airport
 out_file = open("graph","w")
 out_file_id=open("graph_id", "w")
-for airport_code in airports_array.keys():
-	#routes=req_flights(airport_code)
-	routes=req_flights("MLC")
+out_file_id.write("# source_id destination_id weight\n")
+num=0
+#while the queue is not empty
+while not airports_queue.full():
+	num=num+1
+	#get the next airport id, and ask for its routes
+	airport_code=airports_queue.get()
+	routes=req_flights(airport_code)
 	edges=parse_routes(routes)
+	#write all the newly found routes
 	for j in edges.items():
 		out_file.write(airport_code+"\t"+j[0]+"\t"+str(j[1])+"\n")
-		out_file_id.write(str(airports_array[airport_code])+"\t"+str(airports_array[j[0]])+"\t"+str(j[1])+"\n")
-		
-	break
-	
+		#if a new airport is found, assign it a new private id number and put it in the queue
+		if not airports_map.has_key(j[0]):
+			airports_map[j[0]]=len(airports_map)
+			airports_queue.put(j[0])
+		out_file_id.write(str(airports_map[airport_code])+"\t"+str(airports_map[j[0]])+"\t"+str(j[1])+"\n")
+	print(str(num))
 out_file.close()
 out_file_id.close()
+
+#also write to file the dictionary with iata code-private id association
+out_file = open("pages/airports","w")
+out_file.write(str(airports_map))
+out_file.close()
