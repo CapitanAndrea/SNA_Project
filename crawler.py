66043eb68385db096397e9ec602f2e85e334f126
atanga14 <atanga14@gmail.com>
2017-06-09 19:46:40 +0200

    Add files via upload
    .) update first value (inserted 'barabasi');
    problem: crawler case sensitive.

--- crawler.py
+++ crawler.py
@@ -122,9 +122,9 @@ if not os.path.exists('pages'):
     os.makedirs('pages')
 
 page_map={}
-page_map['Tamas_Vicsek']=0
+page_map['barabasi']=0
 page_queue=Queue.Queue()
-page_queue.put('Tamas_Vicsek')
+page_queue.put('barabasi')
 
 out_file = open("graph","w")
 out_file_id=open("graph_id", "w")
